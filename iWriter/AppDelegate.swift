15fa65305a5b40e4f0e132b48d20b3791e89d73f
jiangyouhua <jingo1997@163.com>
2019-07-09 10:42:08 +0800

    Initial Commit

---
+++ iWriter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iWriter
+//
+//  Created by Jiangyouhua on 2019/7/9.
+//  Copyright © 2019 Jiangyouhua. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iWriter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iWriter
+//
+//  Created by Jiangyouhua on 2019/7/9.
+//  Copyright © 2019 Jiangyouhua. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
